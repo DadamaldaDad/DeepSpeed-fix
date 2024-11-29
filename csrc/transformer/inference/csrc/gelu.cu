2bf580ecea5df60cc312fad9f8d575873d7731d3
DadamaldaDad <97605267+DadamaldaDad@users.noreply.github.com>
2024-11-29 12:28:02 +0100

    Fix gelu.cu

--- csrc/transformer/inference/csrc/gelu.cu
+++ csrc/transformer/inference/csrc/gelu.cu
@@ -12,9 +12,9 @@ namespace cg = cooperative_groups;
 #define MAX_SEQ 2048
 
 // only used to avoid compilation error due to lack of definition.
-#ifndef BF16_AVAILABLE
-using __nv_bfloat162 = __half2;
-#endif
+// #ifndef BF16_AVAILABLE
+// using __nv_bfloat162 = __half2;
+// #endif
 
 inline __device__ float gelu(const float x)
 {
